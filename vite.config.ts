f1376f1ae5200a07e212227109b331c8ca0e5f43
Adepoju Ayotomiwa Adelowo <adelowotomiwa2020@gmail.com>
2026-09-20 16:37:25 +0100

    Configure deployment for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Impeccabletech-Portfolio/",
+  base: process.env.VERCEL ? "/" : "/Impeccabletech-Portfolio/",
   plugins: [react()],
 });
